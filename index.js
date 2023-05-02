e0e12b2ba8c11842bd02feb81d943e2a852b7936
zhuanya28 <azhukova1402@gmail.com>
2023-05-02 11:00:20 +0800

    added: other types of shapes

--- index.js
+++ index.js
@@ -19,7 +19,114 @@ function draw() {
   cam.loadPixels(); //cam.pixels is accesible after this line!
   let w = cam.width;
   let h = cam.height;
-  vertexes(w, h);
+  trigs(w, h);
+}
+
+function trigs(w, h) {
+  for (let y = 0; y < h; y += RESOLUTION) {
+    stroke(255);
+    //fill(255, 100);
+    noFill();
+    for (let x = 0; x < w; x += RESOLUTION) {
+      let index = (x + y * w) * 4; // RGBA
+      let r = cam.pixels[index + 0];
+      let g = cam.pixels[index + 1];
+      let b = cam.pixels[index + 2];
+      let a = cam.pixels[index + 3];
+
+      let avg = (r + g + b) / 3;
+
+      // let adjSize = map(avg, 0, 255, 0, RESOLUTION);
+      // image(img, adjSize, adjSize);
+      // // stroke(map(mouseX, 0, width, 0, 255), 0, 200);
+
+      fill(
+        map(x, 0, w, 0, 255),
+        map(y, 0, h, 0, 255),
+        map(RESOLUTION, 0, 20, 0, 255),
+        80
+      );
+      let size = map(avg, 0, 255, 0, RESOLUTION);
+      triangle(
+        x,
+        y + random(-RESOLUTION - 10, RESOLUTION + 10),
+        x + random(-RESOLUTION - 10, RESOLUTION + 10),
+        y,
+        x,
+        y + random(-RESOLUTION - 10, RESOLUTION + 10)
+      );
+    }
+  }
+}
+
+function circles(w, h) {
+  for (let y = 0; y < h; y += RESOLUTION) {
+    stroke(255);
+    //fill(255, 100);
+    noFill();
+    for (let x = 0; x < w; x += RESOLUTION) {
+      let index = (x + y * w) * 4; // RGBA
+      let r = cam.pixels[index + 0];
+      let g = cam.pixels[index + 1];
+      let b = cam.pixels[index + 2];
+      let a = cam.pixels[index + 3];
+
+      let avg = (r + g + b) / 3;
+
+      // let adjSize = map(avg, 0, 255, 0, RESOLUTION);
+      // image(img, adjSize, adjSize);
+      // // stroke(map(mouseX, 0, width, 0, 255), 0, 200);
+
+      stroke(
+        map(x, 0, w, 0, 255),
+        map(y, 0, h, 0, 255),
+        map(RESOLUTION, 0, 20, 0, 255)
+      );
+      fill(
+        map(x, 0, w, 0, 255),
+        map(y, 0, h, 0, 255),
+        map(RESOLUTION, 0, 20, 0, 255),
+        20
+      );
+      let size = map(avg, 0, 255, 0, RESOLUTION);
+      circle(x, y, size);
+    }
+  }
+}
+
+function rectangles(w, h) {
+  for (let y = 0; y < h; y += RESOLUTION) {
+    stroke(255);
+    //fill(255, 100);
+    noFill();
+    for (let x = 0; x < w; x += RESOLUTION) {
+      let index = (x + y * w) * 4; // RGBA
+      let r = cam.pixels[index + 0];
+      let g = cam.pixels[index + 1];
+      let b = cam.pixels[index + 2];
+      let a = cam.pixels[index + 3];
+
+      let avg = (r + g + b) / 3;
+
+      // let adjSize = map(avg, 0, 255, 0, RESOLUTION);
+      // image(img, adjSize, adjSize);
+      // // stroke(map(mouseX, 0, width, 0, 255), 0, 200);
+
+      stroke(
+        map(x, 0, w, 0, 255),
+        map(y, 0, h, 0, 255),
+        map(RESOLUTION, 0, 20, 0, 255)
+      );
+      fill(
+        map(x, 0, w, 0, 255),
+        map(y, 0, h, 0, 255),
+        map(RESOLUTION, 0, 20, 0, 255),
+        20
+      );
+      let size = map(avg, 0, 255, 0, RESOLUTION);
+      rect(x, y, size, size);
+    }
+  }
 }
 
 function vertexes(w, h) {
@@ -54,9 +161,9 @@ function vertexes(w, h) {
 }
 function mouseWheel(event) {
   print(event.delta);
-  if (event.delta > 0 && RESOLUTION < 20) {
+  if (event.delta > 0 && RESOLUTION < 40) {
     RESOLUTION += 1;
-  } else if (event.delta < 0 && RESOLUTION > 3) {
+  } else if (event.delta < 0 && RESOLUTION > 8) {
     RESOLUTION -= 1;
   }
 }
