54dcaaf43c3400953774b4285bf33e7d6e6a5e7a
zhuanya28 <azhukova1402@gmail.com>
2023-05-07 21:47:35 +0800

    added: vertex

--- vertex.js
+++ vertex.js
@@ -1,8 +1,6 @@
 let RESOLUTION = 16;
 let cam;
 
-let currentMode = 1;
-
 function setup() {
   let canvas = createCanvas(windowWidth, 480);
   canvas.parent("canvasContainer");
@@ -15,7 +13,7 @@ function setup() {
 function draw() {
   background(255);
 
-  cam.loadPixels(); //cam.pixels is accesible after this line!
+  cam.loadPixels();
   let w = cam.width;
   let h = cam.height;
 
@@ -24,10 +22,6 @@ function draw() {
 
 function vertexes(w, h) {
   for (let y = 0; y < h; y += RESOLUTION) {
-    stroke(255);
-    //fill(255, 100);
-    noFill();
-    beginShape();
     for (
       let x = (windowWidth - w) / 2;
       x < (windowWidth + w) / 2;
@@ -37,18 +31,25 @@ function vertexes(w, h) {
       let r = cam.pixels[index + 0];
       let g = cam.pixels[index + 1];
       let b = cam.pixels[index + 2];
-      let a = cam.pixels[index + 3];
+      let mapX = map(x, 0, w, 0, width);
+      let mapY = map(y, 0, h, 0, (width * 3) / 4);
 
-      let avg = (r + g + b) / 3;
-
-      // let adjSize = map(avg, 0, 255, 0, RESOLUTION);
-      // image(img, adjSize, adjSize);
-      // // stroke(map(mouseX, 0, width, 0, 255), 0, 200);
-      stroke(map(avg, 150, 255, 0, 255));
-      let adjY = map(avg, 0, 255, -RESOLUTION, RESOLUTION);
-      vertex(x, y + adjY);
+      //noFill();
+      //stroke(avg);
+      noStroke();
+      let colorDiff = 10;
+      fill(
+        r + random(-colorDiff, colorDiff),
+        g + random(-colorDiff, colorDiff),
+        b
+      ) + random(-colorDiff, colorDiff);
+      beginShape();
+      let len = map(avg, 0, 255, 10, 50);
+      vertex(mapX + random(-len, len), mapY + random(-len, len));
+      vertex(mapX + random(-len, len), mapY + random(-len, len));
+      vertex(mapX + random(-len, len), mapY + random(-len, len));
+      endShape(CLOSE);
     }
-    endShape();
   }
 }
 
